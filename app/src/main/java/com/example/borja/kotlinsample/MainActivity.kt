b063f4a4d6ef1ea5ea7335fd8f69c1d66980b5a4
borja <borjapg616@gmail.com>
2017-03-14 22:12:55 +0100

    Anko extension to simplify how to find RecyclerView

--- app/src/main/java/com/example/borja/kotlinsample/MainActivity.kt
+++ app/src/main/java/com/example/borja/kotlinsample/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.example.borja.kotlinsample
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.widget.Toast
+import org.jetbrains.anko.find
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        val forecastList: RecyclerView = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
-        forecastList.adapter=ForecastListAdapter(items)
-
+        forecastList.adapter = ForecastListAdapter(items)
+        toast("Hello World!")
     }
 
     private val items = listOf(
@@ -25,4 +28,8 @@ class MainActivity : AppCompatActivity() {
             "Sab 6/23",
             "Dom 6/23"
     )
+
+    fun Context.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(this, message, duration).show();
+    }
 }
