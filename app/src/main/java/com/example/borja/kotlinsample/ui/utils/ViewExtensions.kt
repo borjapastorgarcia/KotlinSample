68290f1a7f18e56b481076908f320282f55d77ab
borja <borjapg616@gmail.com>
2017-05-04 22:55:34 +0200

    Making the list clickable

---
+++ app/src/main/java/com/example/borja/kotlinsample/ui/utils/ViewExtensions.kt
@@ -0,0 +1,10 @@
+package com.example.borja.kotlinsample.ui.utils
+
+import android.content.Context
+import android.view.View
+
+/**
+ * Created by borja on 04/05/2017.
+ */
+val View.ctx: Context
+    get() = context
