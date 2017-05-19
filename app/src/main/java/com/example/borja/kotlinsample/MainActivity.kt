d19ce5719fbeeabef695df7975af61bde1e0199e
borja <borjapg616@gmail.com>
2017-05-19 21:10:56 +0200

    Adding Lambdas functions

--- app/src/main/java/com/example/borja/kotlinsample/MainActivity.kt
+++ app/src/main/java/com/example/borja/kotlinsample/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.borja.kotlinsample
 
-import android.content.Context
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.widget.Toast
 import com.example.borja.kotlinsample.commands.RequestForecastCommand
-import com.example.borja.kotlinsample.domain.model.Forecast
-import org.jetbrains.anko.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.find
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,12 +23,7 @@ class MainActivity : AppCompatActivity() {
         doAsync {
             val result = RequestForecastCommand("94043").execute()
             uiThread {
-                forecastList.adapter = ForecastListAdapter(result,
-                        object : ForecastListAdapter.OnItemClickListener {
-                            override fun invoke(forecast: Forecast) {
-                                toast(forecast.date)
-                            }
-                        })
+                val adapter = ForecastListAdapter(result) { toast(it.date) }
             }
         }
     }
