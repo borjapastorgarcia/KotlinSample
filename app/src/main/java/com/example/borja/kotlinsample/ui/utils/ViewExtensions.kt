a95313272c3f464d006f71f76c845489124f1681
borja <borjapg616@gmail.com>
2017-09-12 19:03:44 +0200

    Delegated Properties

--- app/src/main/java/com/example/borja/kotlinsample/ui/utils/ViewExtensions.kt
+++ app/src/main/java/com/example/borja/kotlinsample/ui/utils/ViewExtensions.kt
@@ -3,8 +3,6 @@ package com.example.borja.kotlinsample.ui.utils
 import android.content.Context
 import android.view.View
 
-/**
- * Created by borja on 04/05/2017.
- */
+
 val View.ctx: Context
     get() = context
